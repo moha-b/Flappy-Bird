be355b8dcfec93e583179a0b50b7e677f57f75a4
hab <Mohabzax4@gmail.com>
2022-08-19 14:09:37 +0200

    bird is jumping
    add new image, create audio folder, reorganize the code

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,10 +1,6 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace, avoid_unnecessary_containers
 import 'dart:async';
-
 import 'package:flutter/material.dart';
-
-import 'bird.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -14,80 +10,110 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  static double birdy=0;
-  double time=0;
-  double height=0;
-  double startheight=birdy;
-  bool gamehasStarted=false;
-  void jump(){
-    setState(() {
-      time=0;
-      startheight=birdy;
+
+  static double yAxis = 0;
+  double time = 0;
+  double height = 0;
+  double initialHeight = yAxis;
+  bool gameHasStarted = false;
+
+  // TODO:
+  void jump() {
+    setState((){
+      time = 0;
+      initialHeight = yAxis;
+
     });
 
   }
-  void startgame(){
-    gamehasStarted=true;
-    Timer.periodic(Duration(microseconds:60 ), (timer) {
-      time+=0.05;
-      height=-4.9*time*time+2.8*time;
-      setState(() {
-        birdy=startheight-height;
+  // TODO:
+  void startGame(){
+    gameHasStarted = true;
+    Timer.periodic(Duration(milliseconds: 100), (timer) {
+      time += 0.05;
+      height = -4.9 * time * time + 2.8 * time;
+      setState((){
+        yAxis = initialHeight - height;
       });
-      if(birdy>1){
+      if(yAxis > 3.5){
         timer.cancel();
-        gamehasStarted=false;
-      }
+        gameHasStarted = false;
+      };
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
-      body: Stack(
-        children: [
-          GestureDetector(
-            onTap: (){
-              if(gamehasStarted){
-                jump();
-              }
-              else{
-                startgame();
-              }
-            },
-            child: AnimatedContainer(
-              alignment: Alignment(0,birdy),
-              duration: Duration(microseconds: 0),
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                      fit: BoxFit.fill, image: AssetImage("assets/pics/background-day.png"))),
-              child: bird(),
-
-            ),
-          ),
-          Align(
-            alignment: Alignment.topLeft,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
+      body: Column(
+          children: [
+            Expanded(
+              flex: 2,
+                child: Stack(
                   children: [
-                    Text("Score",style: TextStyle(color: Colors.white.withOpacity(0.5),fontSize: 20), ),
-                    Text("0",style: TextStyle(color: Colors.white.withOpacity(0.5),fontSize: 20), ),
+                    GestureDetector(
+                      onTap: (){
+                        if(gameHasStarted){
+                          jump();
+                        }else{
+                          startGame();
+                        }
+                      },
+                      child: AnimatedContainer(
+                          alignment: Alignment(0,yAxis),
+                          duration: Duration(milliseconds: 0),
+                          color: Colors.blue,
+                          child: bird()),
+                    ),
+                    // Container(
+                    //   child: Text("TAP TO START"),
+                    // ),
                   ],
                 ),
-                Column(
-                  children: [
-                    Text("Best",style: TextStyle(color: Colors.white.withOpacity(0.5),fontSize: 20), ),
-                    Text("10",style: TextStyle(color: Colors.white.withOpacity(0.5),fontSize: 20), ),
-                  ],
-                )
-              ],
             ),
-          ),
-        ],
-      ),
+            Container(
+              height: 15,
+              color: Colors.green,
+            ),
+            Expanded(
+                flex: 1,
+                child: Container(color: Colors.brown,child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                      Text("Score",style: TextStyle(color: Colors.white,fontSize: 30),),
+                      SizedBox(height: 10,),
+                      Text("0",style: TextStyle(color: Colors.white,fontSize: 30)),
+                    ],),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                      Text("Best",style: TextStyle(color: Colors.white,fontSize: 30)),
+                      SizedBox(height: 10,),
+                      Text("0",style: TextStyle(color: Colors.white,fontSize: 30)),
+                    ],),
+                  ],
+                ),)),
 
+          ]),
+    );
+  }
+Container bird(){
+    return Container(
+        height: 130,
+        width: 130 ,
+        child: Image.asset("assets/pics/bird.png",)
+    );;
+}
+  Container barrier(double size){
+    return Container(
+        height: 130,
+        width: size,
+        decoration: BoxDecoration(
+          color: Colors.green,
+        ),
     );
   }
 }
