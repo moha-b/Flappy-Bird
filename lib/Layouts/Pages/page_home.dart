4c8b869d1bb61a10707e4dce97dc0f61113d95ea
hab <Mohabzax4@gmail.com>
2022-09-04 20:00:46 +0200

    finished

--- lib/Layouts/Pages/page_home.dart
+++ lib/Layouts/Pages/page_home.dart
@@ -106,7 +106,6 @@ class _HomePageState extends State<HomePage> {
         }
       });
       if (birdIsDead()) {
-
         timer.cancel();
         _showDialog();
       }
@@ -169,6 +168,7 @@ class _HomePageState extends State<HomePage> {
 
     showDialog(
       context: context,
+      barrierDismissible: false,
       builder: (context) {
         return AlertDialog(
           backgroundColor: Colors.white,
